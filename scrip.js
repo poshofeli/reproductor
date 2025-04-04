21468d28dc0661e4b2b7d41b54422fdebcbb528e
eduardo <jesusedu050@gmial.com>
2025-04-04 17:06:56 -0400

    revisar

---
+++ scrip.js
@@ -0,0 +1,29 @@
+
+const contenedor = document.getElementById("musicas")
+
+function peticion(info) {
+
+    const musicas = info.data.songs
+
+    musicas.map(renderizarMusica)
+
+}
+
+function renderizarMusica(info) {
+
+    const div = document.createElement("div")
+    div.setAttribute("class", "")
+
+    div.innerHTML = `
+    <img src="/assets/Frame 5.svg" alt="">
+            <h3>musica</h3>
+            `
+    contenedor.appendChild(div)
+
+}
+
+
+axios.get("https://api.institutoalfa.org/api/songs")
+    .then(peticion)
+
+
