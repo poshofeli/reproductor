a9829bfb8f7c62b7c5981e05fca41869333fefb7
poshofeli <jesusedu050@gmail.com>
2025-04-09 19:59:34 +0000

    60

--- scrip.js
+++ scrip.js
@@ -17,11 +17,20 @@ function renderizarMusica(info) {
     div.innerHTML = `
     <img src="${info.image.url}" alt="">
             <h3>${info.title}</h3>
-
             `
+    div.addEventListener("click", function (){
+        sonar(info)})
     contenedor.appendChild(div)
 
-} 
+}
+
+function sonar(info) {
+    const tiempo = document.getElementById("tiempo").setAttribute("src" , info.audio.url)
+
+   document.getElementById("image").setAttribute("src" , info.image.url)
+
+   document.getElementById("titulo").innerHTML =info.title
+}
 
 
 
